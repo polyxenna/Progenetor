c5a7c080ad39266e2184aeaae4797727851480d0
Bianca Jessa A. Carabio <carabiobiancajessa@gmail.com>
2024-12-04 14:14:37 +0800

    modified gemini version

--- components/Progenetor.jsx
+++ components/Progenetor.jsx
@@ -24,7 +24,7 @@ const Progenetor = () => {
     const genAI = new GoogleGenerativeAI(apiKey);
 
     const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash-latest",
+        model: "gemini-1.5-flash",
     });
 
     const generationConfig = {
